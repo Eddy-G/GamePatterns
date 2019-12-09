6c0f5f522c167deefc6ddfc5146f6545fb1fa75b
Eddy <eddy.garnier34@gmail.com>
2019-12-09 14:20:16 +0200

    First import

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    #region Singleton
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("There is already a BuildManager in the scene!");
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public GameObject standardTurretPrefab;
+    public GameObject missileLauncherPrefab;
+
+    public GameObject buildEffect;
+
+    private TurretBlueprint turretToBuild;
+
+    public bool canBuild { get { return turretToBuild != null; } }
+    public bool hasMoney { get { return PlayerStats.money >= turretToBuild.cost; } }
+
+    public void BuildTurretOn(Node node)
+    {
+        if(PlayerStats.money < turretToBuild.cost)
+        {
+            Debug.Log("Not enough money for that");
+            return;
+        }
+
+        PlayerStats.money -= turretToBuild.cost;
+
+        GameObject turret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
+        node.turret = turret;
+
+        GameObject effect = (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 1f);
+
+        Debug.Log("The object has been bought, you have left:" + PlayerStats.money);
+    }
+
+    public void SelectTurretToBuild(TurretBlueprint turret)
+    {
+        turretToBuild = turret;
+    }
+
+}
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+    public TurretBlueprint standardTurret;
+    public TurretBlueprint missileLauncherTurret;
+
+    private BuildManager buildManager;
+
+    private void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+    public void SelectStandardTurret()
+    {
+        Debug.Log("Standard turret selected");
+        buildManager.SelectTurretToBuild(standardTurret);
+    }
+
+    public void SelectMissileLauncher()
+    {
+        Debug.Log("lance rocket selected");
+        buildManager.SelectTurretToBuild(missileLauncherTurret);
+    }
+}
